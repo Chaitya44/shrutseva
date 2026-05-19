499c43a1cb446b311d3bab258b4e04f8bbf311b3
Chaitya44 <Chaitya44@users.noreply.github.com>
2026-05-19 17:41:25 +0530

    chore: remove unnecessary temporary test files and error logs

--- phpinfo.php
+++
@@ -1,5 +0,0 @@
-<?php
-echo "Current Directory: " . __DIR__ . "<br>";
-echo "Document Root: " . $_SERVER['DOCUMENT_ROOT'] . "<br>";
-echo "Active script: " . $_SERVER['SCRIPT_FILENAME'] . "<br>";
-?>
