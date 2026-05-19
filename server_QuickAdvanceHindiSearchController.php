42bdbaad697b7fc1d6ede6000a4c200002f5c971
Chaitya44 <Chaitya44@users.noreply.github.com>
2026-05-19 17:16:32 +0530

    Remove Hindi bypass filter check in Hindi search controller as well

--- server_QuickAdvanceHindiSearchController.php
+++ server_QuickAdvanceHindiSearchController.php
@@ -150,9 +150,7 @@ class QuickAdvanceHindiSearchController extends BaseController
             if (isset($langMap[$langVal])) {
                 $langVal = $langMap[$langVal];
             }
-            if ($langVal !== 'H') {
-                $queryList->where('master.language', 'like', $langVal . '%');
-            }
+            $queryList->where('master.language', 'like', $langVal . '%');
         }
 
         if (!empty($request->subject)) {
@@ -356,15 +354,13 @@ class QuickAdvanceHindiSearchController extends BaseController
             if (isset($langMap[$langVal])) {
                 $langVal = $langMap[$langVal];
             }
-            if ($langVal !== 'H') {
-                if ($includeRelevant) {
-                    $search = $this->normalizeSearchTerm($langVal);
-                    $queryCount->whereRaw("(master.language LIKE ? OR master.language regexp ? OR master.language regexp ?)", [$langVal . "%", $langVal . "%", $search . "%"]);
-                    $queryList->whereRaw("(master.language LIKE ? OR master.language regexp ? OR master.language regexp ?)", [$langVal . "%", $langVal . "%", $search . "%"]);
-                } else {
-                    $queryCount->where('master.language', 'like', $langVal . "%");
-                    $queryList->where('master.language', 'like', $langVal . "%");
-                }
+            if ($includeRelevant) {
+                $search = $this->normalizeSearchTerm($langVal);
+                $queryCount->whereRaw("(master.language LIKE ? OR master.language regexp ? OR master.language regexp ?)", [$langVal . "%", $langVal . "%", $search . "%"]);
+                $queryList->whereRaw("(master.language LIKE ? OR master.language regexp ? OR master.language regexp ?)", [$langVal . "%", $langVal . "%", $search . "%"]);
+            } else {
+                $queryCount->where('master.language', 'like', $langVal . "%");
+                $queryList->where('master.language', 'like', $langVal . "%");
             }
         }
 
