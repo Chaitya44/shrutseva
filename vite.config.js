41741a98598ecaf76892410f8216e6c6b334c54f
Chaitya44 <Chaitya44@users.noreply.github.com>
2026-05-19 14:57:11 +0530

    feat: add Vite server proxy and Vercel rewrites proxying requests to live database server

--- vite.config.js
+++ vite.config.js
@@ -5,4 +5,20 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  server: {
+    proxy: {
+      '/front': {
+        target: 'http://147.93.155.190/test',
+        changeOrigin: true,
+      },
+      '/api': {
+        target: 'http://147.93.155.190/test',
+        changeOrigin: true,
+      },
+      '/get_bhandars_list': {
+        target: 'http://147.93.155.190/test',
+        changeOrigin: true,
+      }
+    }
+  }
 })
